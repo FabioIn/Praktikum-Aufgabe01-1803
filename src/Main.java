50c196e82cb308b8c85bafde03d3b130221f713a
FabioIn <inter.fabi@outlook.de>
2025-03-21 12:29:51 +0100

    changed into 2D-Array and added Bubble sorting

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,48 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        String[] personsOriginal = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-        String[] personsCloned = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        String[] personsOriginal = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+        String[] personsCloned = personsOriginal.clone();  // Kopie der Original-Liste
         String[] shuffledList = new String[personsCloned.length];
-        int [] exercises = new int[shuffledList.length];
+        int[][] table = new int[shuffledList.length][2]; // Zweidimensionales Array für Person + Aufgabe
+        int [] cache = new int[2];
+
         int exercisesNumber = 5;
         Random rand = new Random();
-        int taskNumber = 0;
 
-        //Personen mischen
+        // Personen mischen (Fisher-Yates Shuffle)
         for (int i = 0; i < personsCloned.length; i++) {
             int randIndex;
             do {
                 randIndex = rand.nextInt(personsCloned.length);
-            } while (personsCloned[randIndex].equals("null"));
+            } while (personsCloned[randIndex].equals("USED"));
 
             shuffledList[i] = personsCloned[randIndex];
-            personsCloned[randIndex] = "null";
+            personsCloned[randIndex] = "USED";  // Markieren, um nicht erneut verwendet zu werden
         }
 
-        //Aufgabe zuweisen
-        for (int i = 0; i < personsCloned.length - 1; i++) {
-            taskNumber = (i % exercisesNumber) + 1;  // Aufgaben von 1 bis 5 wiederholen
-            System.out.println(shuffledList[i] + " hat die Aufgabe " + taskNumber);
+        // Aufgaben zufällig zuweisen (rotierend von 1 bis 5)
+        for (int i = 0; i < shuffledList.length; i++) {
+            table[i][0] = i;  // Speichert den Index der Person
+            table[i][1] = (i % exercisesNumber) + 1;  // Aufgabe von 1 bis 5
         }
 
+        // Bubble Sort nach Aufgaben sortieren
+        for (int i = 0; i < table.length - 1; i++) { // Durchlaufschleife
+
+            for (int r = 0; r < table.length - 1 - i; r++) { //Vergleichsschleife
+                if (table[r][1] > table[r + 1][1]) {
+                    cache= table[r];
+                    table[r] = table[r + 1];
+                    table[r + 1] = cache;
+                }
+            }
+        }
+
+        // Ausgabe der sortierten Liste
+        System.out.println("Die Aufgaben werden wie folgt verteilt:");
+        for (int i = 0; i < table.length; i++) {
+            System.out.print(shuffledList[table[i][0]] + ": " + table[i][1]+ " ; ");
+        }
     }
 }
