616cd6926d5da85ce0b1f3b723219a1a6d7b4d7c
FabioIn <inter.fabi@outlook.de>
2025-03-21 12:39:17 +0100

    Added comments and made the fine tune

--- src/Main.java
+++ src/Main.java
@@ -2,48 +2,50 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        //Deklaration
         String[] personsOriginal = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
         String[] personsCloned = personsOriginal.clone();  // Kopie der Original-Liste
         String[] shuffledList = new String[personsCloned.length];
         int[][] table = new int[shuffledList.length][2]; // Zweidimensionales Array für Person + Aufgabe
         int [] cache = new int[2];
-
         int exercisesNumber = 5;
         Random rand = new Random();
 
-        // Personen mischen (Fisher-Yates Shuffle)
+        // Personen mischen
         for (int i = 0; i < personsCloned.length; i++) {
             int randIndex;
             do {
                 randIndex = rand.nextInt(personsCloned.length);
-            } while (personsCloned[randIndex].equals("USED"));
+            } while (personsCloned[randIndex].equals("null"));  //Überprüfung, ob Person bereits verwendet wurde
 
             shuffledList[i] = personsCloned[randIndex];
-            personsCloned[randIndex] = "USED";  // Markieren, um nicht erneut verwendet zu werden
+            personsCloned[randIndex] = "null";  // Markieren, um nicht erneut verwendet zu werden
         }
 
-        // Aufgaben zufällig zuweisen (rotierend von 1 bis 5)
+        // Aufgaben der Reihe nach zuweisen (rotierend von 1 bis 5)
         for (int i = 0; i < shuffledList.length; i++) {
-            table[i][0] = i;  // Speichert den Index der Person
-            table[i][1] = (i % exercisesNumber) + 1;  // Aufgabe von 1 bis 5
+            table[i][0] = i;  // Spalte 0: Speichert den Index der Person
+            table[i][1] = (i % exercisesNumber) + 1;  // Spalte 1: Speichert die zugewiesene Aufgabe von 1 bis 5
         }
 
         // Bubble Sort nach Aufgaben sortieren
-        for (int i = 0; i < table.length - 1; i++) { // Durchlaufschleife
+        // Index r steht für Row
+        for (int i = 0; i < table.length - 1; i++) { // Durchlaufschleife: Steuert, wie oft iteriert werden soll
 
-            for (int r = 0; r < table.length - 1 - i; r++) { //Vergleichsschleife
-                if (table[r][1] > table[r + 1][1]) {
-                    cache= table[r];
-                    table[r] = table[r + 1];
-                    table[r + 1] = cache;
+            for (int r = 0; r < table.length - 1 - i; r++) {    //Vergleichsschleife: Wir iterieren Zeile für Zeile durch und vergleichen jeweils die benachbarten Zeilen miteinander
+                if (table[r][1] > table[r + 1][1]) {            //[r] und [r+1] sind benachbarte Zeilen
+                    cache= table[r];                            //Wenn, dann wird die Zeile (1D-Array) in cache (ebenfalls 1D-Array) zwischengespeichert
+                    table[r] = table[r + 1];                    //Zeilentausch
+                    table[r + 1] = cache;                       //Zeilentausch mh von cache
                 }
             }
+
         }
 
         // Ausgabe der sortierten Liste
         System.out.println("Die Aufgaben werden wie folgt verteilt:");
         for (int i = 0; i < table.length; i++) {
-            System.out.print(shuffledList[table[i][0]] + ": " + table[i][1]+ " ; ");
+            System.out.print(shuffledList[table[i][0]] + ": " + table[i][1]+ " | ");
         }
     }
 }
